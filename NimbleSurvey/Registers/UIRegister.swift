ee588bbaa8ee107cbfa66e2ffd4b59104fc01aa1
Mark G <markgravity.in@gmail.com>
2020-10-31 19:49:50 +0700

    chore: style navigation bar

---
+++ NimbleSurvey/Registers/UIRegister.swift
@@ -0,0 +1,15 @@
+//
+//  UIRegister.swift
+//  NimbleSurvey
+//
+//  Created by Mark G on 31/10/2020.
+//
+
+import SwiftyBase
+
+class UIRegister: NSObject, PluginRegisterable {
+    
+    func configure(application: UIApplication, launchOptions: [UIApplication.LaunchOptionsKey : Any]?) {
+        
+    }
+}
