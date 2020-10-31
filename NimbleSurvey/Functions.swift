a5b82ab7d3dfc50b294afaa3f4ab0e69539e9f5e
Mark G <markgravity.in@gmail.com>
2020-10-31 19:33:58 +0700

    chore: add localization

--- NimbleSurvey/Functions.swift
+++ NimbleSurvey/Functions.swift
@@ -10,10 +10,16 @@ import SwiftyBase
 
 func alert(_ message: String, handler: VoidHandler? = nil) {
     let controller = UIAlertController(
-        title: "alert.title_text",
+        title: "alert.title_text".trans(),
         message: message,
         preferredStyle: .alert
     )
+    
+    // Ok
+    let action = UIAlertAction(title: "alert.ok_text".trans(), style: .cancel) { _ in
+        handler?()
+    }
+    controller.addAction(action)
     
     guard let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first
     else { return }
